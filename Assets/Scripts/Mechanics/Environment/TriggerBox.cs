4965f3ca87fa3ea98c4bfbb0b091912542a77fc1
Chandler <chandlerqvan@gmail.com>
2025-07-17 14:59:33 -0700

    Created Reusable Trigger Box
    with configerable targeting
    - Layers
    - Tags

---
+++ Assets/Scripts/Mechanics/Environment/TriggerBox.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.UI;
+
+public class TriggerBox : MonoBehaviour
+{
+    [Header("Target Identifiers")]
+    public LayerMask targetLayers;
+    public string[] targetTags;
+
+    [Header("Callbacks")]
+    public UnityEvent<Collider2D> OnEnter;
+    public UnityEvent<Collider2D> OnStay;
+    public UnityEvent<Collider2D> OnExit;
+
+    [Header("Debug")]
+    public bool DoDebug = false;
+
+    private void Awake()
+    {
+        if (!TryGetComponent(out Collider2D _) && DoDebug) Debug.LogWarning("TriggerBox Missing Collider");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(IsTarget(collision))
+            OnEnter?.Invoke(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (IsTarget(collision))
+            OnStay?.Invoke(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (IsTarget(collision))
+            OnExit?.Invoke(collision);
+    }
+
+    private bool IsTarget(Collider2D collision)
+    {
+        bool foundTag = targetTags.Length == 0;
+
+        if (!foundTag)
+            foreach (string tag in targetTags)
+                if (collision.CompareTag(tag))
+                {
+                    foundTag = true;
+                    break;
+                }
+
+        if (!foundTag)
+        {
+            if (DoDebug) Debug.Log($"{collision.name}: was not on target tags");
+            return false;
+        }
+
+        if (targetLayers != 0)
+            if ((targetLayers & (1 << collision.gameObject.layer)) == 0)
+            {
+                if (DoDebug) Debug.Log($"{collision.name}: was not on target layers");
+                return false;
+            }
+
+        return true;
+    }
+}
