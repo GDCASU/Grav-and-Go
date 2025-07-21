e2951920ae5338acffee11f2932e5b9b5adbebac
Chandler <chandlerqvan@gmail.com>
2025-07-21 02:20:44 -0700

    Created temp UI for system
    - works with rich text
    - has indicator for next
    - might make an example idk

--- Assets/Scripts/Fundamentals/Managers/InputManager.cs
+++ Assets/Scripts/Fundamentals/Managers/InputManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 /// <summary>
-/// Singleton that owns the <c>PlayerControls</c> asset and surfaces the
+/// Singleton that compiles all user input and surfaces the
 /// current input state each frame.
 /// </summary>
 public class InputManager : MonoBehaviour
--- Assets/Scripts/UI/Monologue/MonologueManager.cs
+++ Assets/Scripts/UI/Monologue/MonologueManager.cs
@@ -1,10 +1,51 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using System.Collections;
 
 public class MonologueManager : MonoBehaviour
 {
-    public TextAsset test;
+    [Header("References")]
+    public TMP_Text textLabel;
+    public GameObject nextImage;
+    public GameObject chatContainer;
+
+    [Header("Typing Settings")]
+    public int typingSpeed = 15;
+
+    private UnityEvent onMonologSkip;
+    private bool monologing = false;
+
+    private Coroutine currentLineCoro = null;
+    private string[] currentMonolog = { };
+    private string currentLine;
+    private int currentLineIndex = 0;
+
+    public static MonologueManager Instance;
+
+    #region MonoBehaviour Functions
+
+    private void Awake()
+    {
+        /* -------- Singleton enforcement -------- */
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        // might add on left click to here later who knows
+        InputManager.OnInteract += ContinueMonolog;
+    }
+
+    #endregion
+
+    #region Input Binding
+
+    #endregion
 
     #region Text Binding Functions
 
@@ -17,7 +58,7 @@ public class MonologueManager : MonoBehaviour
 
     public void UnloadTextBinding(string key)
     {
-        if(!textBindings.ContainsKey(key)) { throw new KeyNotFoundException($"{key} was not found as a text binding"); }
+        if (!textBindings.ContainsKey(key)) { throw new KeyNotFoundException($"{key} was not found as a text binding"); }
 
         textBindings[key] = null;
     }
@@ -33,10 +74,84 @@ public class MonologueManager : MonoBehaviour
 
     #region Monologue Functions
 
-    public string[] LoadMonologueFile(TextAsset monolog)
+    /// <summary>
+    /// Starts a Monolog that freezes time
+    /// </summary>
+    /// <param name="text">the target monolog TextAsset file</param>
+    public void StartMonolog(TextAsset text)
+    {
+        if (monologing)
+        {
+            Debug.LogWarning($"Tried to Start this monolog when monolog \"{text.name}\"already started", text);
+            return;
+        }
+
+        monologing = true;
+
+        // apply visual effects here
+        Time.timeScale = 0.0f;
+        chatContainer.SetActive(true);
+
+        // load monolog and start first line
+        currentMonolog = LoadMonologueFile(text);
+        currentLineIndex = 0;
+
+        textLabel.text = "";
+        currentLineCoro = StartCoroutine(TypeLine());
+    }
+
+    private void ContinueMonolog()
+    {
+        if (!monologing) return;
+
+        if(currentLineCoro != null) // skip typing animation if still running
+        {
+            StopCoroutine(currentLineCoro);
+            textLabel.text = currentLine;
+            currentLineCoro = null;
+            currentLineIndex++;
+
+            nextImage.SetActive(true);
+            return;
+        }
+        else // move onto the next line if not
+        {
+            // if it was the last line end the monolog
+            if (currentLineIndex >= currentMonolog.Length)
+            {
+                monologing = false;
+                Time.timeScale = 1;
+                chatContainer.SetActive(false);
+                return;
+            }
+
+            // else start a the next line
+            textLabel.text = "";
+            currentLineCoro = StartCoroutine(TypeLine());
+        }
+    }
+
+    private IEnumerator TypeLine()
+    {
+        currentLine = InterpretLine(currentMonolog[currentLineIndex]);
+        nextImage.SetActive(false);
+
+        for (int i = 0; i < currentLine.Length; i++)
+        {
+            textLabel.text += currentLine[i];
+            yield return new WaitForSecondsRealtime(1f / typingSpeed);
+        }
+
+        nextImage.SetActive(true);
+        currentLineCoro = null;
+        currentLineIndex++;
+    }
+
+    private string[] LoadMonologueFile(TextAsset monolog)
     {
         string[] lines = monolog.text.Split('\n');
 
+        // add \n to the text
         foreach(string line in lines)
             while (line.Contains("\\n")) line.Replace("\\n", "\n"); 
 
@@ -52,12 +167,12 @@ public class MonologueManager : MonoBehaviour
 
         while(textBindingsIndex > -1)
         {
+            // find full text binding
             int bindingEndIndex = finalLine.IndexOf('}', textBindingsIndex + 2);
             if (bindingEndIndex == -1) throw new Exception($"Unclosed textBindings found in line \"{line}\"");
 
             // get text binding key
             string key = finalLine.Substring(textBindingsIndex + 2, bindingEndIndex - textBindingsIndex - 2);
-
 
             string result = CallTextBinding(key);
 
@@ -71,6 +186,6 @@ public class MonologueManager : MonoBehaviour
 
         return string.Format(finalLine, bindingResults.ToArray());
     }
-
+    
     #endregion
 }
