6bd5c1387b338f6b9e83280d657837014dff8a5e
Alfaroqomar <117319313+Alfaroqomar@users.noreply.github.com>
2025-09-26 15:34:21 -0700

    V1/breakaway boxes/omar (#16)
    Temporary finish for breakaway boxes -- useful for breakaways in the future.
    
    * WIP
    
    changes frm 9/19?
    
    * boxes breakable

---
+++ Assets/Scripts/Mechanics/Environment/BreakableBoxes.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class BreakableBoxes : MonoBehaviour, IDamageable
+{
+
+    private GameObject box;
+    private int maxHealth = 3;
+    private int currentHealth;
+    private bool destroyed = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+
+
+
+    private void throwEffects()
+    {
+        //release particles maybe play coroutine
+        //play sound
+    }
+
+
+    private void boxDied()
+    {
+        destroyed = true;
+        throwEffects();
+        Destroy(gameObject);
+        // play sound
+        // play animation
+        // spawn loot
+        //Destroy(box);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0 && !destroyed)
+        {
+            boxDied();
+        }
+    }
+}
