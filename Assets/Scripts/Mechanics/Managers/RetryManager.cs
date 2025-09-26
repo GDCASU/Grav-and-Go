8007ee69a6e35474206a5726b38ffce5af78dce5
Century178 <mrothenberger0918@gmail.com>
2025-09-26 15:12:03 -0700

    Update RetryManager.cs

--- Assets/Scripts/Mechanics/Managers/RetryManager.cs
+++ Assets/Scripts/Mechanics/Managers/RetryManager.cs
@@ -3,16 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class RetryManager : MonoBehaviour
 {
+    public static RetryManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
     public void OnDeath(GameObject deadVictim)
     {
         gameObject.BroadcastMessage("OnDeath", gameObject);
 
-        if (deadVictim.TryGetComponent(out PlayerMovementController player)) RetryUI();
-
-        Retry();
+        if (deadVictim.TryGetComponent(out PlayerMovementController player))
+        {
+            OnRetry();
+            RetryUI();
+        }
     }
 
-    public void Retry()
+    public void OnRetry()
     {
         //Reloads the whole level. Temporary solution; replace with LoadSave() (or the equivalent) when the saving system is done.
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
