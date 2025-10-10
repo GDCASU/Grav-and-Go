e4de08b4d6f11f4dc27815db547d138cf57cdd13
YashvijayS <Singhyashvijay@gmail.com>
2025-10-10 16:19:17 -0700

    Update DoorScript.cs

--- Assets/Scenes/Features/Yashvijay Singh/DoorScript.cs
+++ Assets/Scenes/Features/Yashvijay Singh/DoorScript.cs
@@ -7,8 +7,12 @@ public class DoorScript : MonoBehaviour
     private bool _doorXrotate =false;
     private bool _doorYrotate = false;
     public bool PlayerCloseEnough = false;
-    private float _closeEnoughDistance = 1;
+    private float _closeEnoughDistance = 2;
+    private int _countClose = 0; // used so door only rotates once when player is near
+    private int _countFar = 1; // used to rotate door back when player no longer close
     public PlayerMovementController Player;  
+
+    // now give it collision i guess? 
 
     void Start()
     {
@@ -20,22 +24,36 @@ public class DoorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
         if (Vector2.Distance(Player.transform.position, transform.position) < _closeEnoughDistance)//checks if player close to door
         {
             PlayerCloseEnough = true;
 
         }
+        else
+        {
+            PlayerCloseEnough = false;
+        }
         
-        if (PlayerCloseEnough)
+
+        if (PlayerCloseEnough && (_countClose < 1))
         {
             doorRotateY();
+            _countClose += 1;
+            _countFar = 0;
+
+        }
+        if (!PlayerCloseEnough && (_countFar < 1))
+        {
+            doorRotateY();
+            _countClose = 0;
+            _countFar += 1;
         }
     }
 
     void doorRotateY() // make door rotate 90 degrees about the y axis
     {
-        _rotateAmt = 45;
+        _rotateAmt = 90;
         transform.Rotate(Vector3.up, _rotateAmt);
         
     }
