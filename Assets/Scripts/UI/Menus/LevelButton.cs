3a9e04562b6675f1e1a3936aec66b39879f25123
ntibn <67208103+ntibn@users.noreply.github.com>
2025-11-21 15:39:02 -0700

    V1/level select screen/jacob rebekah (#47)
    * First
    
    * Commit 1
    
    * Fix
    
    ---------
    
    Co-authored-by: camilee03 <crlee15@asu.edu>

--- Assets/Scripts/UI/Menus/LevelButton.cs
+++ Assets/Scripts/UI/Menus/LevelButton.cs
@@ -19,22 +19,19 @@ public class LevelButton : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Button _levelButton;
-    
     [Header("Target Level")]
-    [SerializeField] private Level _levelReference;
-    
+    [SerializeField] public LevelName _levelReference;
     [Header("Debugging")]
     [SerializeField] private bool _doDebugLog;
-    
+
     private void Start()
     {
         // Check if the level is unlocked
         bool isUnlocked = LevelManager.Instance.IsLevelUnlocked(_levelReference);
-        
         // Set if the button is interactable
         _levelButton.interactable = isUnlocked;
     }
-    
+
     /// <summary>
     /// Function that the UI button uses to load the level
     /// </summary>
@@ -44,4 +41,5 @@ public class LevelButton : MonoBehaviour
         // HACK: Make sure to set timescale to 1 in case the game was exited from a pause
         Time.timeScale = 1f;
     }
+    
 }
