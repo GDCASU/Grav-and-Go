2de99104ee4728d440214f53b46c2a114d934684
camilee03 <crlee15@asu.edu>
2025-11-21 15:37:41 -0700

    Connect with Death/Health

--- Assets/AirMine.cs
+++ Assets/AirMine.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines.ExtrusionShapes;
@@ -6,23 +7,27 @@ public class AirMine : MonoBehaviour
 {
     public float explodeRadius;
     public float detectRadius;
+    ParticleSystem.ShapeModule shape;
 
     private void Awake() {
         GetComponent<CircleCollider2D>().radius = detectRadius;
-        var shape = GetComponent<ParticleSystem>().shape;
-        shape.radius = detectRadius;
+        shape = GetComponent<ParticleSystem>().shape;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        shape.radius = detectRadius;
+
         Debug.Log("COLLISION1");
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explodeRadius);
         
         for(int i = 0; i<colliders.Length; i++)
         {
             //deal damage to these objects
+            if (colliders[i].CompareTag("Player")) DeathManager.TriggerPlayerDeath();
         }
 
+        // Note: may want to replace with an explosion animation later
         Destroy(gameObject);
     }
 }
