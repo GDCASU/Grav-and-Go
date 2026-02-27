566f9df73e29425a9a04bb39c960728f32f6e858
Josh Wright <awsomebash22@gmail.com>
2026-02-27 14:39:57 -0700

    gravity well fix

--- Assets/Scripts/Mechanics/Environment/AOE cylinder/EffectList.cs
+++ Assets/Scripts/Mechanics/Environment/AOE cylinder/EffectList.cs
@@ -7,7 +7,8 @@ using UnityEngine;
  * Author:
  * Davyd Yehudin
  * 
- * Modified By:
+ * Modified By: Justin Miller
+ * Joshua Wright
  * 
  */// --------------------------------------------------------
 
@@ -19,11 +20,13 @@ public class EffectList
     // Use this bool to gate all your Debug.Log Statements please
     [Header("Debugging")]
     [SerializeField] private bool _doDebugLog;
-    
+
     //effect0 (upward force)
-    public static void effect0(GameObject a){
+    public static void effect0(GameObject a)
+    {
         Rigidbody2D forced;
-        if(!a.TryGetComponent<Rigidbody2D>(out forced)){
+        if (!a.TryGetComponent<Rigidbody2D>(out forced))
+        {
             Debug.Log("(((((");
             return;
         }
@@ -33,10 +36,12 @@ public class EffectList
         forced.AddForce(force);
         Debug.Log(a.name);
     }
-    //effect1 (sideways force)
-    public static void effect1(GameObject a){
+    //effect1 (Right force)
+    public static void effect1(GameObject a)
+    {
         Rigidbody2D forced;
-        if(!a.TryGetComponent<Rigidbody2D>(out forced)){
+        if (!a.TryGetComponent<Rigidbody2D>(out forced))
+        {
             Debug.Log("(((((");
             return;
         }
@@ -47,10 +52,70 @@ public class EffectList
         Debug.Log(a.name);
     }
 
+    //effect2 (Downward force)
+    public static void effect2(GameObject a)
+    {
+        Rigidbody2D forced;
+        if (!a.TryGetComponent<Rigidbody2D>(out forced))
+        {
+            Debug.Log("(((((");
+            return;
+        }
+        Vector2 force;
+        force.x = 0;
+        force.y = -100;
+        forced.AddForce(force);
+        Debug.Log(a.name);
+    }
+
+    //effect3 (Left force)
+    public static void effect3(GameObject a)
+    {
+        Rigidbody2D forced;
+        if (!a.TryGetComponent<Rigidbody2D>(out forced))
+        {
+            Debug.Log("(((((");
+            return;
+        }
+        Vector2 force;
+        force.y = 0;
+        force.x = -100;
+        forced.AddForce(force);
+        Debug.Log(a.name);
+    }
+    
+    //Gravity Well (Brings to Center)
+    public static void GravityWell(GameObject pulled, Collider2D towards, float thrust)
+    {
+        Rigidbody2D rb;
+        if (!pulled.TryGetComponent<Rigidbody2D>(out rb))
+        {
+            Debug.Log("(((((");
+            return;
+        }
+        Vector2 force;
+
+        // Calculate the direction vector from the current object to the target
+        Vector2 direction = (Vector2)towards.bounds.center - (Vector2)pulled.transform.position;
+
+        // Normalize the vector to get a consistent magnitude (length of 1)
+        // This ensures the force applied is the same regardless of distance
+        direction.Normalize();
+
+        // Apply force in that direction, multiplied by the thrust value
+        force = direction * thrust;
+        rb.AddForce(force);
+        Debug.Log(pulled.name);
+    }
+
     //effects 0 and 1 are examples, change if necessary
 }
 //enum of all effects, don't forget to add the new effect here and then add it to the switch statement in AOE_cylinder
-public enum effectsEnum{
+public enum effectsEnum
+{
     effect0,
-    effect1
+    effect1,
+    effect2,
+    effect3,
+    GravityWell
 }
